14148305acdd7df4a7d69645f9c72d2cc4390c25
danialzivehdar <243804543+danialzivehdadr@users.noreply.github.com>
2026-07-26 00:11:47 +0330

    Initialize perf/serve-compile-cache-defer-version

---
+++ packages/web-shell/client/index.ts
@@ -0,0 +1,33 @@
+export { App as WebShell } from './App';
+export type { WebShellProps, BugReportInfo } from './App';
+export type { ComposerToolbarAction } from './components/ChatEditor';
+export type { ToastTone } from './components/ToastHost';
+export type { WebShellLanguage } from './i18n';
+export type {
+  MarkdownContentSource,
+  MarkdownRenderContext,
+  ToolHeaderExtraRenderer,
+  ToolHeaderExtraRenderInfo,
+  ToolHeaderKind,
+  ComposerToolbarStartRenderer,
+  WelcomeFooterRenderer,
+  WebShellComposerApi,
+  WebShellComposerInput,
+  WebShellComposerTag,
+  WebShellComposerTagOptions,
+  WebShellComposerTagPlacement,
+  WebShellComposerToolbarStartRenderInfo,
+  WebShellComposerTextOptions,
+  WelcomeHeaderRenderer,
+  WebShellMarkdownCustomization,
+  WebShellFooterRenderInfo,
+  FooterRenderer,
+  LoadingPhrasesResolver,
+  WebShellTaskInfo,
+  WebShellAgentTask,
+  WebShellShellTask,
+  WebShellMonitorTask,
+  WebShellModelInfo,
+  WebShellSkillInfo,
+} from './customization';
+export type { WelcomeHeaderProps } from './components/WelcomeHeader';
