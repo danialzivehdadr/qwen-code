4c4313af4fd05fe9a950573516552df8c837635e
danialzivehdar <243804543+danialzivehdadr@users.noreply.github.com>
2026-07-26 00:53:39 +0330

    Initialize release/v0.19.6-preview.0

---
+++ packages/web-shell/client/index.ts
@@ -0,0 +1,64 @@
+export { App as WebShell } from './App';
+export type { WebShellProps, BugReportInfo, SessionChangeEvent } from './App';
+export type { ComposerToolbarAction } from './components/ChatEditor';
+export type { ToastTone } from './components/ToastHost';
+export type { WebShellLanguage } from './i18n';
+export type { WebShellTheme } from './themeContext';
+export type {
+  CodeBlockRenderer,
+  MarkdownContentSource,
+  MarkdownRenderContext,
+  MarkdownTableMode,
+  ToolHeaderExtraRenderer,
+  ToolHeaderExtraRenderInfo,
+  ToolHeaderKind,
+  UserMessageContentRenderer,
+  UserMessageContentRenderInfo,
+  ComposerToolbarStartRenderer,
+  ComposerToolbarRightRenderer,
+  WelcomeFooterRenderer,
+  WebShellComposerApi,
+  WebShellBuiltinComposerTagKind,
+  WebShellComposerInput,
+  WebShellComposerTag,
+  WebShellComposerTagIconMap,
+  WebShellComposerTagKind,
+  WebShellComposerTagOptions,
+  WebShellComposerTagPlacement,
+  WebShellComposerToolbarRenderInfo,
+  WebShellComposerToolbarStartRenderInfo,
+  WebShellComposerToolbarRightRenderInfo,
+  WebShellComposerTextOptions,
+  WelcomeHeaderRenderer,
+  WebShellMarkdownCustomization,
+  WebShellFooterRenderInfo,
+  FooterRenderer,
+  LoadingPhrasesResolver,
+  WebShellAtItem,
+  WebShellAtProvider,
+  WebShellCodeBlockRenderInfo,
+  WebShellTaskInfo,
+  WebShellAgentTask,
+  WebShellShellTask,
+  WebShellMonitorTask,
+  WebShellModelInfo,
+  WebShellSkillInfo,
+} from './customization';
+export type { WelcomeHeaderProps } from './components/WelcomeHeader';
+export {
+  ECHARTS_FULLDATA_LANGUAGE,
+  EchartsFullDataBlock,
+  createEchartsFullDataRenderer,
+} from './components/messages/EchartsFullDataBlock';
+export type {
+  DatasetCell,
+  EchartsFullDataBlockProps,
+  EchartsFullDataOption,
+  EchartsFullDataRefMeta,
+  EchartsFullDataRefResolver,
+  EchartsFullDataResolvedDataset,
+  EchartsFullDataRendererOptions,
+  EchartsInstance,
+  EchartsRuntime,
+  EchartsRuntimeLoader,
+} from './components/messages/EchartsFullDataBlock';
